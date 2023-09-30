023c6c0ea0e3c109f442d74d7fb288543c8d8bd6
NicolasPozzer <pozzernico@gmail.com>
2023-09-30 17:48:06 -0300

    publish in git

---
+++ src/test/java/com/demo/CRUD_AUTO/CrudAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.CRUD_AUTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
